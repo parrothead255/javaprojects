3e7aa94807bdf19672eefdd1e09f51c3846ad5bd
parrothead255 <parrothead255@gmail.com>
2020-09-04 09:14:10 -0500

    java

---
+++ Chap03/src/Chap302.java
@@ -0,0 +1,13 @@
+
+public class Chap302 {
+	public static void main(String args[]) {
+		int a = 1;
+		int e = a + 4;
+		long key = 100000;
+		
+		System.out.print(a+e);
+		System.out.print(key);
+
+	}
+			
+}
