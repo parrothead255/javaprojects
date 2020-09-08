31897240f3e7be7d2b5274e9f25caba41108e222
parrothead255 <parrothead255@gmail.com>
2020-09-08 10:45:35 -0500

    java

---
+++ Chap03/src/Chap303.java
@@ -0,0 +1,25 @@
+
+public class Chap303 {
+	public static void main(String args[]) {
+		
+		int shrek = 0;
+		int donkey = 0;
+		int butterPants = 0;
+		System.out.println(shrek+" + "+donkey+" = "+ butterPants);
+		butterPants = shrek + donkey;
+		System.out.println(shrek+" + "+donkey+" = "+ butterPants);
+		butterPants = shrek + donkey;
+		System.out.println(shrek+" + "+donkey+" = "+ butterPants);
+		butterPants = shrek - donkey;
+		System.out.println(shrek+" - "+donkey+" = "+ butterPants);
+		butterPants = shrek*donkey;
+		System.out.println(shrek+" * "+donkey+" = "+ butterPants);
+		butterPants = shrek / donkey;
+		System.out.println(shrek+" / "+donkey+" = "+ butterPants);
+		butterPants = shrek % donkey;
+		System.out.println(shrek+" % "+donkey+" = "+ butterPants);
+		
+		
+		
+	}
+}
---
+++ Chap03/src/Chap304.java
@@ -0,0 +1,11 @@
+
+public class Chap304 {
+public static void main(String args[]) {
+		double sean = 10;
+		double hayden=-3.3333333333333333;
+		double race=0.2020;
+		System.out.println(sean+" + "+hayden+" = "+race);
+		race=sean+hayden;
+		
+	}
+}
