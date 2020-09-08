db099a7dde74d5fc99f00dd09f7ad9d2771f26a3
parrothead255 <parrothead255@gmail.com>
2020-09-08 10:55:08 -0500

    Create Chap305.java

---
+++ Chap03/src/Chap305.java
@@ -0,0 +1,27 @@
+
+public class Chap305 {
+
+	public static void main(String args[]) {
+		int boston=1000;
+		System.out.println(boston);
+		boston=boston+1000;
+		System.out.println(boston);
+		boston=boston*1000;
+		System.out.println(boston);
+		boston=boston*1000;
+		System.out.println(boston);	
+		boston=boston*1000;
+		System.out.println(boston);	
+		double boris=1.123456789;
+		System.out.println(boris);
+		boris=1.1234567890123456;
+		System.out.println(boris);	
+		boris=1.12345678901234567890;
+		System.out.println(boris);
+		double peace=10;
+		boris=3;
+		double result=peace/boris;
+		System.out.println(result);
+		
+	}
+}
